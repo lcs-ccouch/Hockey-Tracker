f08d21a20de35c41d9b5fa259ac28015c913edac
Calum Couch <ccouch@lcs.on.ca>
2023-06-01 14:55:57 -0400

    Initial Commit

---
+++ Hockey Tracker/Hockey_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hockey_TrackerApp.swift
+//  Hockey Tracker
+//
+//  Created by calum couch on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct Hockey_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
